3915c0bfcdc94e0b75f082ada44c9de150f98042
Chris Bond <BeheadedKamikaze@users.noreply.github.com>
2021-09-11 04:01:16 +1000

    Parent split cameras to an Empty

--- SplitCamera.py
+++ SplitCamera.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy.types import Camera, Context, Object, Operator, Scene
+from bpy.types import Camera, Collection, Context, Object, Operator, Scene
 from typing import List
 
 from .SRR_Settings import SRR_Settings
@@ -19,7 +19,7 @@ class SRR_OT_SplitCamera(Operator):
     def execute(self, context: Context):
         active_object: Object = bpy.context.active_object
         if not active_object or active_object.type != 'CAMERA':
-            ShowMessageBox("You must select a camera as the active object.", title="Error", icon='WARN')
+            ShowMessageBox("You must select a camera as the active object.", title="Error", icon='ERROR')
             return {'CANCELLED'}
 
         self.saved_settings = save_render_settings(context, camera_object=active_object)
@@ -29,21 +29,36 @@ class SRR_OT_SplitCamera(Operator):
             ShowMessageBox("No tiles to render.")
             return {'CANCELLED'}
 
-        for tile in self.tiles:
-            print(str(tile))
-            self.split_camera(context, camera_object=active_object, tile=tile)
+        # create an Empty to parent all the new cameras onto
+        parent_empty = self.make_empty(context, camera_object=active_object)
 
-        self.report({'INFO'}, "Selected camera has been split")
+        # split cameras
+        new_cameras: List[Object] = []
+        for tile in self.tiles:
+            new_camera = self.split_camera(context, camera_object=active_object, tile=tile)
+            new_cameras.append(new_camera)
+
+        # parent the cameras onto the Empty
+        self.deselect_all(context)
+        for camera in new_cameras:
+            camera.select_set(True)
+        parent_empty.select_set(True)
+        context.view_layer.objects.active = parent_empty
+        bpy.ops.object.parent_set(type='OBJECT')
+
+        ShowMessageBox(f"Selected camera has been split into {len(new_cameras)} new cameras")
 
         return {'FINISHED'}
 
-    def split_camera(self, context: Context, camera_object: Object, tile: RenderTile):
+    def deselect_all(self, context: Context):
+        for obj in context.view_layer.objects.selected:
+            obj: Object
+            obj.select_set(False)
+
+    def split_camera(self, context: Context, camera_object: Object, tile: RenderTile) -> Object:
         tile_settings: TileCameraSplitSettings = tile.tile_settings
 
-        # deselect everything
-        for o in context.view_layer.objects.selected:
-            o: Object
-            o.select_set(False)
+        self.deselect_all(context)
 
         # select original camera
         camera_object.select_set(True)
@@ -53,7 +68,7 @@ class SRR_OT_SplitCamera(Operator):
         bpy.ops.object.duplicate()
 
         # apply settings to new camera
-        new_camera = context.view_layer.objects.active
+        new_camera: Object = context.view_layer.objects.active
         new_camera.name = tile_settings.camera_name
 
         camera_data: Camera = new_camera.data
@@ -62,3 +77,34 @@ class SRR_OT_SplitCamera(Operator):
         camera_data.dof.aperture_fstop = tile_settings.fstop
         camera_data.shift_x = tile_settings.shift_x
         camera_data.shift_y = tile_settings.shift_y
+
+        return new_camera
+
+    def make_empty(self, context: Context, camera_object: Object) -> Object:
+        # put the empty in the same location as the original camera
+        camera_location = camera_object.location
+        bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=camera_location, scale=(1, 1, 1))
+        empty: Object = context.view_layer.objects.active
+        empty.name = f"{self.saved_settings.old_camera_name}_Split"
+
+        # link the Empty to all the same collections as the parent camera
+        for collection in camera_object.users_collection:
+            collection: Collection
+            try:
+                collection.objects.link(empty)
+            except RuntimeError:
+                # the object might already be in this collection
+                pass
+
+        # add the Empty to the same parent as the original camera, if it has one
+        if camera_object.parent:
+            parent_object = camera_object.parent
+
+            self.deselect_all(context)
+
+            empty.select_set(True)
+            parent_object.select_set(True)
+            context.view_layer.objects.active = parent_object
+            bpy.ops.object.parent_set(type='OBJECT')
+
+        return empty
